a5d550900b3f096318f51d72dc15bd547c99ccf2
beginor <beginor@qq.com>
2018-08-28 08:50:08 +0800

    change base image to debian:stretch-slim

--- src/install.sh
+++ src/install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
+apt-get update
+apt-get install -y gnupg apt-transport-https dirmngr
 # add mono in vs channel
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/ubuntu vs-xenial main" | tee /etc/apt/sources.list.d/mono-official-vs.list
+echo "deb https://download.mono-project.com/repo/debian vs-stretch main" | tee /etc/apt/sources.list.d/mono-official-vs.list
 # update & upgrade
 apt-get update && apt-get upgrade -y
 # install mono
 apt-get install -y --no-install-recommends mono-devel
 # cleanup
+apt-get remove -y gnupg apt-transport-https dirmngr
 apt-get autoremove -y
 rm -rf /var/lib/apt/lists/*
