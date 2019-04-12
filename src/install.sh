6b5367eaca208dac88a40f5e4163fd91f2e5c3a1
beginor <beginor@qq.com>
2019-04-12 17:35:43 +0800

    update install script

--- src/install.sh
+++ src/install.sh
@@ -2,7 +2,7 @@
 ### every exit != 0 fails the script
 set -e
 apt-get update
-apt-get install -y gnupg apt-transport-https dirmngr
+apt-get install -y --no-install-recommends gnupg apt-transport-https dirmngr
 # add mono in vs channel
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/debian vs-stretch main" | tee /etc/apt/sources.list.d/mono-official-vs.list
